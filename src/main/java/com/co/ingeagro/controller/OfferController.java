a027694a6e93c6e7beb372c67124c2b7b9b20146
AndresLop91 <afelipe-18@hotmail.com>
2023-09-09 15:40:59 -0500

    Api get para obtener los primeros 5 productos

---
+++ src/main/java/com/co/ingeagro/controller/OfferController.java
@@ -0,0 +1,35 @@
+package com.co.ingeagro.controller;
+
+import com.co.ingeagro.data.ProductData;
+import com.co.ingeagro.model.Product;
+import com.co.ingeagro.repository.product.IProductRepository;
+import com.co.ingeagro.repository.product.impl.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/OfferProduct")
+
+
+public class OfferController {
+
+
+
+    @Autowired
+    private IProductRepository productRepository;
+
+    @GetMapping("/first5")
+    public List<ProductData> getFirst5Products() {
+        return productRepository.getAll(PageRequest.of(0, 5)).getContent();
+    }
+
+}
+
+
+
+
